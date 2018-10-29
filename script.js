d09ac4521ef025dd8fd68aec3156b5710f604af2
Joshua Henderson <joshuahenderson031@gmail.com>
2018-10-29 16:39:45 -0700

    Added parallax background images, opacity, transition animations, cleaned up overall look.

--- script.js
+++ script.js
@@ -33,9 +33,9 @@ $("#button3").click(function() {
  // ===== Scroll to Top ==== 
 $(window).scroll(function() {
     if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+        $('#return-to-top').fadeIn();    // Fade in the arrow
     } else {
-        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+        $('#return-to-top').fadeOut();   // Else fade out the arrow
     }
 });
 $('#return-to-top').click(function() {      // When arrow is clicked
