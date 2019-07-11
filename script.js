7b166f115c41977a9d915c5841a8b09af321cd8e
Joshua Henderson <joshuahenderson031@gmail.com>
2019-07-10 18:04:17 -0700

    optimized jquery

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 $(document).ready(function () {
 
+    var $target = $('html,body');
+
     $("#button1").click(function () {
-        $('html, body').animate({
+        $target.stop().animate({
             scrollTop: $("#aboutsection").offset().top
-        }, 800);
+        }, 100);
     });
 
     $("#button2").click(function () {
-        $('html, body').animate({
+        $target.stop().animate({
             scrollTop: $("#portfolio").offset().top
-        }, 800);
+        }, 100);
     });
 
     $("#button3").click(function () {
-        $('html, body').animate({
+        $target.stop().animate({
             scrollTop: $("#contact").offset().top
-        }, 800);
+        }, 100);
     });
 
 
-    // ===== Scroll to Top ==== 
     $(window).scroll(function () {
         if ($(this).scrollTop() >= 50) { // If page is scrolled more than 50px
             $('#return-to-top').fadeIn(); // Fade in the arrow
@@ -27,9 +28,10 @@ $(document).ready(function () {
             $('#return-to-top').fadeOut(); // Else fade out the arrow
         }
     });
+
     $('#return-to-top').click(function () { // When arrow is clicked
-        $('body,html').animate({
+        $('body,html').stop().animate({
             scrollTop: 0 // Scroll to top of body
-        }, 500);
+        }, 100);
     });
 });
